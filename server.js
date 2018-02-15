9cabcc13331442457186301b37efff95f86ce332
PallaviKsai <34658115+PallaviKsai@users.noreply.github.com>
2018-02-15 15:41:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 4200,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 'localhost',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '52.226.18.21',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
